dac85283307394450d2057647ef2a004f7424d10
ABM Mohiuddin <abm26.236@gmail.com>
2026-02-21 15:56:04 +0600

    Remove unnecessary line breaks in index.php

--- api/telegram/index.php
+++ api/telegram/index.php
@@ -1,6 +1,5 @@
 <?php
 header("Content-Type: text/plain; charset=utf-8");
-
 function getTOTP(string $s, int $off = 0): string {
     $a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
     $s = strtoupper(str_replace('=', '', $s));
@@ -36,13 +35,10 @@ for ($i = -1; $i <= 1; $i++) {
         break;
     }
 }
-
 if (!$auth) die("Unauthorized");
-
 $cid = getenv("CHAT_ID");
 $tok = getenv("BOT_TOKEN");
 $txt = esc($_POST["text"] ?? "[Empty]");
-
 $ch = curl_init();
 curl_setopt_array($ch, [
     CURLOPT_URL => "https://api.telegram.org/bot$tok/sendMessage",
@@ -51,5 +47,4 @@ curl_setopt_array($ch, [
     CURLOPT_RETURNTRANSFER => true
 ]);
 $res = json_decode(curl_exec($ch), true);
-
 die($res["ok"] ? "Sent to @{$res["result"]["chat"]["username"]}" : $res["description"]);
